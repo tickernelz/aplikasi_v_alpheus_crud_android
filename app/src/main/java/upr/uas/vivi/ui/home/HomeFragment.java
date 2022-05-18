45a3a76bfad58946211b3dc9b6ad1d3c98da12e9
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-18 10:45:14 +0700

    show produk list in home fragment
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

--- app/src/main/java/upr/uas/vivi/ui/home/HomeFragment.java
+++ app/src/main/java/upr/uas/vivi/ui/home/HomeFragment.java
@@ -4,28 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import upr.uas.vivi.databinding.FragmentHomeBinding;
+import java.util.ArrayList;
+
+import upr.uas.vivi.R;
+import upr.uas.vivi.adapter.HomeAdapter;
+import upr.uas.vivi.db.DBHandler;
+import upr.uas.vivi.object.Produk;
 
 public class HomeFragment extends Fragment {
 
-  private FragmentHomeBinding binding;
+  DBHandler db;
+  ArrayList<Produk> produkList;
+  ListView lvHome;
+  HomeAdapter adapter;
+  TextView tvNoHome;
+  View mView;
 
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View mView = inflater.inflate(R.layout.fragment_home, container, false);
+    db = new DBHandler(requireContext());
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+    produkList = new ArrayList<>();
+    tvNoHome = mView.findViewById(R.id.home_data_subheader_tv);
+    lvHome = mView.findViewById(R.id.home_data_lv);
+    produkList = db.getProdukData();
+    adapter = new HomeAdapter(requireContext(), produkList, db);
+    lvHome.setAdapter(adapter);
+    adapter.notifyDataSetChanged();
 
-    return root;
+    if (!produkList.isEmpty()) {
+      tvNoHome.setVisibility(View.INVISIBLE);
+    } else {
+      tvNoHome.setVisibility(View.VISIBLE);
+    }
+
+    return mView;
   }
 
   @Override
   public void onDestroyView() {
     super.onDestroyView();
-    binding = null;
+    mView = null;
   }
 }
